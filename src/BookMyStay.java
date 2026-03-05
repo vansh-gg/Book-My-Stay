03cb15bd71e509afc4b5bbb3dc67e341962ac314
vansh-gg <vansh1234g@gmail.com>
2026-03-05 15:09:10 +0530

    UC1

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,3 +1,8 @@
 public class BookMyStay{
+    public static void main(String[] args) {
 
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
 }
